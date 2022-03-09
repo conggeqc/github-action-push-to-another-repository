8b440083b3ba5ee45c635eecaea84f66e9ea8f9a
cong.qin <306085175@qq.com>
2022-03-09 18:37:30 +0800

    review

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,58 +4,74 @@ set -e  # if a command fails it stops the execution
 set -u  # script fails if trying to access to an undefined variable
 
 echo "[+] Action start"
-SOURCE_BEFORE_DIRECTORY="${1}"
-SOURCE_DIRECTORY="${2}"
-DESTINATION_GITHUB_USERNAME="${3}"
-DESTINATION_REPOSITORY_NAME="${4}"
-GITHUB_SERVER="${5}"
-USER_EMAIL="${6}"
-USER_NAME="${7}"
-DESTINATION_REPOSITORY_USERNAME="${8}"
-TARGET_BRANCH="${9}"
-COMMIT_MESSAGE="${10}"
-TARGET_DIRECTORY="${11}"
-SNIPS_TOOL_FILE_URL="${12}"
-TARGET_GITHUB_USERNAME_API_SPECS="${13}"
-TARGET_REPOSITORY_NAME_API_SPECS="${14}"
-TARGET_BRANCH_API_SPECS="${15}"
-TARGET_GITHUB_USERNAME_JAVA_SDK="${16}"
-TARGET_REPOSITORY_NAME_JAVA_SDK="${17}"
-TARGET_BRANCH_JAVA_SDK="${18}"
+#SOURCE_BEFORE_DIRECTORY="${1}"
+#SOURCE_DIRECTORY="${2}"
+#DESTINATION_GITHUB_USERNAME="${3}"
+#DESTINATION_REPOSITORY_NAME="${4}"
+#GITHUB_SERVER="${5}"
+#USER_EMAIL="${6}"
+#USER_NAME="${7}"
+#DESTINATION_REPOSITORY_USERNAME="${8}"
+#TARGET_BRANCH="${9}"
+#COMMIT_MESSAGE="${10}"
+#TARGET_DIRECTORY="${11}"
+#SNIPS_TOOL_FILE_URL="${12}"
+#TARGET_GITHUB_USERNAME_API_SPECS="${13}"
+#TARGET_REPOSITORY_NAME_API_SPECS="${14}"
+#TARGET_BRANCH_API_SPECS="${15}"
+#TARGET_GITHUB_USERNAME_JAVA_SDK="${16}"
+#TARGET_REPOSITORY_NAME_JAVA_SDK="${17}"
+#TARGET_BRANCH_JAVA_SDK="${18}"
 
 
-if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
-then
-	DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
-fi
 
-if [ -z "$USER_NAME" ]
-then
-	USER_NAME="$DESTINATION_GITHUB_USERNAME"
-fi
+USER_EMAIL="${1}"
+USER_NAME="${2}"
+COMMIT_MESSAGE="${3}"
+#TARGET_BRANCH="${4}"
+
+TARGET_GITHUB_USERNAME_API_SPECS="${4}"
+TARGET_REPOSITORY_NAME_API_SPECS="${5}"
+TARGET_BRANCH_API_SPECS="${6}"
+TARGET_GITHUB_USERNAME_JAVA_SDK="${7}"
+TARGET_REPOSITORY_NAME_JAVA_SDK="${8}"
+TARGET_BRANCH_JAVA_SDK="${9}"
+
+
+
+
+#if [ -z "$DESTINATION_REPOSITORY_USERNAME" ]
+#then
+#	DESTINATION_REPOSITORY_USERNAME="$DESTINATION_GITHUB_USERNAME"
+#fi
+#
+#if [ -z "$USER_NAME" ]
+#then
+#	USER_NAME="$DESTINATION_GITHUB_USERNAME"
+#fi
 
 CLONE_DIR=$(mktemp -d)
 
-TEMP_DIR=$(mktemp -d)
+#TEMP_DIR=$(mktemp -d)
 
 # Setup git
-#git config --global user.email "$USER_EMAIL"
-#git config --global user.name "$USER_NAME"
+git config --global user.email "$USER_EMAIL"
+git config --global user.name "$USER_NAME"
 
-git config --global user.name  "conggeqc"
-git config --global user.email  "conggeqc@gmail.com"
+#git config --global user.name  "conggeqc"
+#git config --global user.email  "conggeqc@gmail.com"
 
 
 
 echo "[+] Cloning destination git repository apispecs java"
 #clone api-specs 
 #echo "clone api-specs" 
-git clone --single-branch --branch "$TARGET_BRANCH_API_SPECS" "https://conggeqc:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_API_SPECS/$TARGET_REPOSITORY_NAME_API_SPECS.git" "$CLONE_DIR"/qingcloud-api-specs
+git clone --single-branch --branch "$TARGET_BRANCH_API_SPECS" "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_API_SPECS/$TARGET_REPOSITORY_NAME_API_SPECS.git" "$CLONE_DIR"/qingcloud-api-specs
 ls -la "$CLONE_DIR"
 
 #clone  java sdk (DESTINATION)
 echo "clone  java sdk"
-git clone --single-branch --branch "$TARGET_BRANCH_JAVA_SDK" "https://conggeqc:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_JAVA_SDK/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" "$CLONE_DIR"/qingcloud-sdk-java
+git clone --single-branch --branch "$TARGET_BRANCH_JAVA_SDK" "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_JAVA_SDK/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" "$CLONE_DIR"/qingcloud-sdk-java
 #ls -la $CLONE_DIR/qingcloud-sdk-java/.git/
 #mkdir "$TEMP_DIR/qingcloud-sdk-java-temp"
 #mv "$CLONE_DIR/qingcloud-sdk-java/.git" "$TEMP_DIR/qingcloud-sdk-java-temp/.git"
@@ -90,10 +106,8 @@ cat .git/config
 #git config --system --unset credential.helper
 
 ORIGIN_COMMIT="https://$GITHUB_SERVER/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-#COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
-#COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
-
-COMMIT_MESSAGE="See ORIGIN_COMMIT from $GITHUB_REF"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/ORIGIN_COMMIT/$ORIGIN_COMMIT}"
+COMMIT_MESSAGE="${COMMIT_MESSAGE/\$GITHUB_REF/$GITHUB_REF}"
 
 
 echo "[+] Adding git commit"
@@ -108,18 +122,15 @@ git diff-index --quiet HEAD || git commit --message "$COMMIT_MESSAGE"
 
 echo "[+] Pushing git commit"
 # --set-upstream: sets de branch when pushing to a branch that does not exist
-#git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$DESTINATION_REPOSITORY_USERNAME/$DESTINATION_REPOSITORY_NAME.git" --set-upstream "$TARGET_BRANCH"
 
-#git config remote.origin.url 'https://$USER_NAME:$API_TOKEN_GITHUB@github.com/${{ github.repository }}'
-
-git remote set-url origin "https://conggeqc:$API_TOKEN_GITHUB@github.com/conggeqc/$TARGET_REPOSITORY_NAME_JAVA_SDK.git"
-git config remote.origin.url "https://conggeqc:$API_TOKEN_GITHUB@github.com/conggeqc/$TARGET_REPOSITORY_NAME_JAVA_SDK.git"
+git remote set-url origin "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/conggeqc/$TARGET_REPOSITORY_NAME_JAVA_SDK.git"
+git config remote.origin.url "https://$USER_NAME:$API_TOKEN_GITHUB@github.com/conggeqc/$TARGET_REPOSITORY_NAME_JAVA_SDK.git"
 
 #echo "cat git config"
 #cat .git/config
 
 
 #git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_JAVA_SDK/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" --set-upstream "$TARGET_BRANCH_JAVA_SDK"
-git push "https://conggeqc:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_JAVA_SDK/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" --set-upstream "$TARGET_BRANCH_JAVA_SDK"
+git push "https://$USER_NAME:$API_TOKEN_GITHUB@$GITHUB_SERVER/$TARGET_GITHUB_USERNAME_JAVA_SDK/$TARGET_REPOSITORY_NAME_JAVA_SDK.git" --set-upstream "$TARGET_BRANCH_JAVA_SDK"
   
 
